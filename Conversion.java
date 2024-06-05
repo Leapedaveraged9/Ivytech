57fd3f3ba710f16d86eee65fe2b13029bbf52904
Leapedaverage9 <gamergangsquad112@gmail.com>
2024-06-04 23:30:31 -0400

    Update

--- Conversion.java
+++ Conversion.java
@@ -1,17 +1,17 @@
 public class Conversion {
     
-    /** Convert from feet to meters */
+    // convert from feet to meters
     public static double footToMeter(double foot) {
         return 0.305 * foot;
     }
 
-    /** Convert from meters to feet */
+    // convert from meters to feet
     public static double meterToFoot(double meter) {
         return 3.279 * meter;
     }
 
     public static void main(String[] args) {
-        // Display tables
+        // display tables
         System.out.println("Feet   Meters   |   Meters   Feet");
         System.out.println("----------------|----------------");
         
